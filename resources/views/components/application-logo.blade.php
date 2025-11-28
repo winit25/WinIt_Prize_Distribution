4c7e82b276dcb0b1a6e2e4a85a6b3a724927550f
User <user@example.com>
2025-11-28 10:17:06 +0100

    Update logo to use SVG version instead of PNG
    - Update sidebar logo to SVG
    - Update login page logo to SVG
    - Update application logo component to SVG
    - Better scalability and smaller file size

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/winit-logo.png') }}" alt="WinIt Logo" {{ $attributes->merge(['class' => 'h-auto']) }} style="max-width: 100%; height: auto;">
+<img src="{{ asset('images/winit-logo.svg') }}" alt="WinIt Logo" {{ $attributes->merge(['class' => 'h-auto']) }} style="max-width: 100%; height: auto;">
